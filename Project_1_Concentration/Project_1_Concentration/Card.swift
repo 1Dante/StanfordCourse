464295114180e8cda2a58720144052d854e2f97b
1Dante <34551416+1Dante@users.noreply.github.com>
2022-10-06 19:03:31 +0300

    added task1

---
+++ Project_1_Concentration/Project_1_Concentration/Card.swift
@@ -0,0 +1,25 @@
+//
+//  Card.swift
+//  Project_1_Concentration
+//
+//  Created by Victor on 15.05.2022.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+
+    static var identifierFactory = 0
+
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
---
+++ Project_1_Concentration/Project_1_Concentration/Concentration.swift
@@ -0,0 +1,48 @@
+//
+//  Concentration.swift
+//  Project_1_Concentration
+//
+//  Created by Victor on 15.05.2022.
+//
+
+import Foundation
+
+class Concentration {
+    var cards: [Card] = []
+    var indexOfoneAndOnlyFaceUpCard: Int?
+    var flipCount = 0
+    var score = 0
+
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+            cards.shuffle()
+        }
+    }
+
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfoneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                    score += 2
+                }
+                cards[index].isFaceUp = true
+                indexOfoneAndOnlyFaceUpCard = nil
+                if !cards[index].isMatched && score > 0 {
+                    score -= 1
+                }
+            } else {
+                //either no cards or 2 cards are face up
+                for flipDownindex in cards.indices {
+                    cards[flipDownindex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfoneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+}
